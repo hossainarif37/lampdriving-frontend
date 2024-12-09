d2406e8c4939685672d9e7287e48eef844eb2f76
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-12-09 15:18:28 +0600

    chore: section heding's heading text lowercase to uppercase

--- src/app/(root)/components/shared/section-heading/SectionHeading.tsx
+++ src/app/(root)/components/shared/section-heading/SectionHeading.tsx
@@ -1,5 +1,4 @@
 import { cn } from '@/lib/utils';
-import { cva } from 'class-variance-authority';
 import { FC, ReactNode } from 'react';
 
 type SectionHeadingProps = {
@@ -17,12 +16,13 @@ const SectionHeading: FC<SectionHeadingProps> = ({children, heading, variant = "
         <div className={cn("flex flex-col items-center text-textCol", classname)}>
 
             {/* heading styles */}
-            <h3 className={cn("bg-secondary py-[3px] px-[15px] text-sm font-bold rounded-[4px] ml-2 relative before:absolute before:w-[5px] before:min-h-full before:bg-secondary text-white before:top-0 before:rounded-[4px] before:-ml-6", headingColor)}>
+            <h3 className={cn("bg-secondary py-[3px] px-[15px] text-sm font-bold rounded-[4px] ml-2 relative before:absolute before:w-[5px] uppercase before:min-h-full before:bg-secondary text-white before:top-0 before:rounded-[4px] before:-ml-6", headingColor)}>
                 {heading}
             </h3>
 
             {/* sub heading styles */}
-            <h1 className={`text-[48px] font-bold text-center ${variant === "default" ? "text-secondary" : "text-white"}`}>       {children}
+            <h1 className={`text-[48px] font-bold text-center ${variant === "default" ? "text-secondary" : "text-white"}`}>       
+                {children}
             </h1>
         </div>
     );
