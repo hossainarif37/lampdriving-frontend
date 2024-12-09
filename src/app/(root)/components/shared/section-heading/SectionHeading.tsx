3181b45b20fdcff3104e4b6b4a63c5134e754a35
mahmudulturan <mahmudulhasanturan@gmail.com>
2024-12-09 14:30:45 +0600

    add: reusable section heading component
    - section heading with default and secondery variant
    - allows to customize style with classname props

---
+++ src/app/(root)/components/shared/section-heading/SectionHeading.tsx
@@ -0,0 +1,31 @@
+import { cn } from '@/lib/utils';
+import { cva } from 'class-variance-authority';
+import { FC, ReactNode } from 'react';
+
+type SectionHeadingProps = {
+    heading: string;
+    children: ReactNode;
+    classname?: string;
+    variant?: "default" | "secondary";
+};
+
+const SectionHeading: FC<SectionHeadingProps> = ({children, heading, variant = "default", classname}) => {
+
+    // set heading color based on the varient
+    const headingColor = variant === "default" ? "bg-secondary before:bg-secondary" : "bg-primary before:bg-primary";
+    return (
+        <div className={cn("flex flex-col items-center text-textCol", classname)}>
+
+            {/* heading styles */}
+            <h3 className={cn("bg-secondary py-[3px] px-[15px] text-sm font-bold rounded-[4px] ml-2 relative before:absolute before:w-[5px] before:min-h-full before:bg-secondary text-white before:top-0 before:rounded-[4px] before:-ml-6", headingColor)}>
+                {heading}
+            </h3>
+
+            {/* sub heading styles */}
+            <h1 className={`text-[48px] font-bold text-center ${variant === "default" ? "text-secondary" : "text-white"}`}>       {children}
+            </h1>
+        </div>
+    );
+};
+
+export default SectionHeading;
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/components/ui/button';
 import React from 'react';
 import { FC } from 'react';
+import SectionHeading from './components/shared/section-heading/SectionHeading';
 
 const HomePage: FC = () => {
   return (
@@ -9,8 +10,14 @@ const HomePage: FC = () => {
       <Button>Button</Button>
       <p className='text-secondary text'>This is the home page</p>
       <div className="min-h-[30vh] bg-secondary">
-        <h3 className='text-[48px] font-bold text-textCol text-center'>Our Range of Services</h3>
+        <SectionHeading classname='items-start py-6 px-3' variant='secondary' heading="Services">
+        Our Range of Services
+        </SectionHeading>
       </div>
+
+      <SectionHeading classname='items-center py-6' heading="Services">
+        Our Range of Services
+        </SectionHeading>
     </div>
   );
 };
