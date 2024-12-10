c0691385543a37435b93cc9acc763f9eb217978a
hossainarif37 <hossainarif4937@gmail.com>
2024-12-10 17:09:11 +0600

    chore: Remove unnecessary wrapper from `Header`

--- src/components/layouts/header/Header.tsx
+++ src/components/layouts/header/Header.tsx
@@ -4,7 +4,7 @@ import Topbar from "./topbar/Topbar";
 const Header = () => {
     return (
         <header className="bg-slate-100 text-center py-4">
-            <div>
+            <div className="wrapper">
                 <Topbar />
                 <Navbar />
             </div>
