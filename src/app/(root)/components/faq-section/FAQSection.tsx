0cdafeda418651acd7fa430879b1b88d53b06912
Hridoy Ahmed <basirulislam.hridoy05@gmail.com>
2024-12-12 22:33:52 +0600

    feat: implement FAQ section using code splitting
    - Added tabs for deferent's FAQ
    - Utilized code splitting for better performance

--- src/app/(root)/components/faq-section/FAQSection.tsx
+++ src/app/(root)/components/faq-section/FAQSection.tsx
@@ -1,33 +1,23 @@
+"use client"
 import { FC } from 'react';
-import {
-    Accordion,
-    AccordionContent,
-    AccordionItem,
-    AccordionTrigger,
-} from "@/components/ui/accordion"
+
 import SectionHeading from '../shared/section-heading/SectionHeading';
+import AccordionDesign from './AccordionDesign';
+import TabsDesign from './TabsDesign';
 
 const FAQSection: FC = () => {
+
     return (
-        <div className=' md:p-32 p-3'>
+        <div className=' md:p-32 p-3 md:space-y-6 space-y-3'>
             {/*Section Heading*/}
             <SectionHeading title='Top Frequently Asked Questions' subtitle='Find answers to common queries about our driving courses, instructors, fees, and more.' />
+            {/*Tabs button part*/}
+            <div>
+                <TabsDesign />
+            </div>
+            {/*Accordion part*/}
             <div className='max-w-4xl mx-auto px-4 md:px-0'>
-                <Accordion type="single" collapsible className="w-full">
-                    <AccordionItem value="item-1">
-                        <AccordionTrigger className='hover:no-underline text-secondary'>Is it accessible?</AccordionTrigger>
-                        <AccordionContent className='text-accent'>
-                            Yes. It adheres to the WAI-ARIA design pattern.
-                        </AccordionContent>
-                    </AccordionItem>
-                    <AccordionItem value="item-2">
-                        <AccordionTrigger>Is it styled?</AccordionTrigger>
-                        <AccordionContent>
-                            Yes. It comes with default styles that matches the other
-                            components&apos; aesthetic.
-                        </AccordionContent>
-                    </AccordionItem>
-                </Accordion>
+                <AccordionDesign />
             </div>
         </div>
     );
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 import HowLampdrivingWorkSection from './components/how-lampdriving-works/HowLampdrivingWorkSection';
 import ChooseBestInstructorSelections from './components/instructor-selection/ChooseBestInstructorSelections';
 import FeaturedInstructors from './components/featured-instructors/FeaturedInstructors';
-import Testimonials from './components/testimials-section/Testimonials';
+import Testimonials from './components/testimonials/Testimonials';
 import AboutUs from './components/about-us-section/AboutUs';
 import FAQSection from './components/faq-section/FAQSection';
 const HomePage: FC = () => {
