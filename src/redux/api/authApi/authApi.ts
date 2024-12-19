0e3fd56dc9ff87b8b57f6f8ae380a3fe9824708e
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2024-12-19 17:49:17 +0600

    chore: declare types on all auth endpoints

--- src/redux/api/authApi/authApi.ts
+++ src/redux/api/authApi/authApi.ts
@@ -1,26 +1,28 @@
 import { ILoginInputs, IRegisterInputs } from "@/types/auth";
 import baseApi from "../baseApi";
+import { IResponseBase, IResponseWithData } from "@/types/response";
+import { IUser } from "@/types/user";
 
 const usersApi = baseApi.injectEndpoints({
     endpoints: (builder) => ({
-        registerUser: builder.mutation<IRegisterInputs, object>({
+        registerUser: builder.mutation<IRegisterInputs, IResponseBase>({
             query: (data) => ({
                 url: '/auth/learners/register',
                 method: "POST",
                 body: data
             })
         }),
-        loginUser: builder.mutation<ILoginInputs, object>({
+        loginUser: builder.mutation<ILoginInputs, IResponseWithData<IUser>>({
             query: (data) => ({
                 url: '/auth/login',
                 method: "POST",
                 body: data
             })
         }),
-        currentUser: builder.query({
+        currentUser: builder.query<void, IResponseWithData<IUser>>({
             query: () => '/user/me'
         }),
-        logOutUser: builder.query({
+        logOutUser: builder.query<void, void>({
             query: () => '/auth/logout'
         })
     })
