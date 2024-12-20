a7d50bf99bba257945086a09d84d289c4dabda04
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2024-12-21 00:16:58 +0600

    add: not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,30 @@
+import { Button } from '@/components/ui/button';
+import { AlertCircle } from 'lucide-react';
+import Link from 'next/link';
+import { FC } from 'react';
+
+const NotFoundPage: FC = () => {
+    return (
+        <div className="min-h-screen bg-gradient-to-b from-purple-50 to-white flex items-center justify-center px-4">
+            <div className="text-center">
+                <div className="flex justify-center mb-8">
+                    <AlertCircle className="w-24 h-24 text-indigo" />
+                </div>
+                <h1 className="text-6xl font-bold text-gray-900 mb-4">404</h1>
+                <h2 className="text-2xl font-semibold text-gray-700 mb-4">Page Not Found</h2>
+                <p className="text-gray-600 mb-8 max-w-md mx-auto">
+                    Oops! The page you're looking for seems to have vanished into thin air.
+                    Let's get you back on track.
+                </p>
+                <Link
+                    href="/"
+                >
+                    <Button className="h-12 w-44 md:w-52 gradient-color">Return Home</Button>
+                    
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default NotFoundPage;
