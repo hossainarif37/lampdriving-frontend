adcdb084978b9c1acccfde7a90a46312d62d2a9b
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2024-12-21 00:40:23 +0600

    Merge branch 'main' of https://github.com/hossainarif37/lampdriving-frontend into feature/errorpage

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -13,14 +13,14 @@ const NotFoundPage: FC = () => {
                 <h1 className="text-6xl font-bold text-gray-900 mb-4">404</h1>
                 <h2 className="text-2xl font-semibold text-gray-700 mb-4">Page Not Found</h2>
                 <p className="text-gray-600 mb-8 max-w-md mx-auto">
-                    Oops! The page you're looking for seems to have vanished into thin air.
-                    Let's get you back on track.
+                    Oops! The page you&apos;re looking for seems to have vanished into thin air.
+                    Let&apos;s get you back on track.
                 </p>
                 <Link
                     href="/"
                 >
                     <Button className="h-12 w-44 md:w-52 gradient-color">Return Home</Button>
-                    
+
                 </Link>
             </div>
         </div>
