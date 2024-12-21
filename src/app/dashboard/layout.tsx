d156cd84853cc362147a05f01ccdad97255a821e
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2024-12-21 19:56:05 +0600

    feat: add dashboard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,11 +1,19 @@
+import Sidebar from '@/components/layouts/sidebar/Sidebar';
 import React, { ReactNode } from 'react';
 import { FC } from 'react';
 
 const DashboardLayout: FC<{ children: ReactNode }> = ({ children }) => {
     return (
-        <div>
-            Dashboard
-            {children}
+        <div className='flex flex-col lg:flex-row'>
+            <div className='lg:min-h-screen lg:w-80 relative'>
+                <Sidebar />
+            </div>
+            <div className='flex-1 min-h-screen border'>
+                {children}
+                <div className="min-h-screen"></div>
+                <div className="min-h-screen"></div>
+                <div className="min-h-screen"></div>
+            </div>
         </div>
     );
 };
