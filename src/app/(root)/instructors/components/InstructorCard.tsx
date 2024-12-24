99758fd25432621cc16868cd6ce6f0a262815a27
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2024-12-24 15:29:12 +0600

    feat: enhance InstructorCard layout and add new instructors to grid

--- src/app/(root)/instructors/components/InstructorCard.tsx
+++ src/app/(root)/instructors/components/InstructorCard.tsx
@@ -25,11 +25,16 @@ interface InstructorCardProps {
 }
 
 const InstructorCard: React.FC<InstructorCardProps> = ({ instructor }) => {
-    const { name, image, rating, experience, pricePerHour, totalRatings,skills, totalLessons } = instructor;
+    const { name, image, rating, experience, pricePerHour, totalRatings, skills, totalLessons } = instructor;
 
 
     return (
-        <div className="p-6 rounded-md space-y-8 bg-gradient-to-b from-primary/0 to-indigo/5">
+        <div className="p-4 rounded-lg space-y-5 bg-gradient-to-b from-primary/0 to-indigo/5 relative">
+            {/* car type */}
+            <span className="px-3 py-1 bg-blue-50 text-blue-600 rounded-full text-sm font-medium flex items-center gap-1 absolute top-5 right-5">
+                <Car className="w-4 h-4" />
+                {skills}
+            </span>
             <div className="flex gap-8">
                 {/* Profile Picture and Auto Badge */}
                 <div className="rounded-full flex flex-col items-center gap-2">
@@ -40,14 +45,19 @@ const InstructorCard: React.FC<InstructorCardProps> = ({ instructor }) => {
                         width={120}
                         className="rounded-full"
                     />
-                    <span className="px-3 py-1 bg-blue-50 text-blue-600 rounded-full text-sm font-medium flex items-center gap-1">
-                        <Car className="w-4 h-4" />
-                        {skills}
-                    </span>
                 </div>
 
                 {/* Profile Details */}
-                <div className="space-y-2 text-sm text-accent">
+                <div className="space-y-2 text-sm text-accent w-full relative">
+
+
+                    {/* Price Per Hour */}
+                    <div>
+                        <h4 className="text-2xl font-bold text-gradient absolute bottom-0 right-2">
+                            ${pricePerHour}<span className="text-base">/hr</span>
+                        </h4>
+                    </div>
+
                     {/* Name */}
                     <h4 className="text-xl font-semibold text-gradient">{name}</h4>
 
@@ -68,42 +78,35 @@ const InstructorCard: React.FC<InstructorCardProps> = ({ instructor }) => {
                     </div>
 
                     {/* Experience */}
-                    <div className="flex items-center font-semibold gap-1">
-                        <Clock className="w-4 h-4 text-primary" />
+                    <div className="flex items-center font-semibold gap-2">
+                        <Clock className="w-5 h-5 text-primary" />
                         <span className="text-gradient">{experience}</span>
-                    </div>
-
-                    {/* Price Per Hour */}
-                    <div>
-                        <h4 className="text-2xl font-bold text-gradient ">
-                            ${pricePerHour}<span className="text-base">/hr</span>
-                        </h4>
                     </div>
                 </div>
             </div>
 
             {/* Action Buttons */}
-            <div className="space-y-4">
-                <div className="flex gap-2 max-w-lg mx-auto">
+            <div className="space-y-3">
+                <div className="flex gap-3 max-w-lg mx-auto">
                     {/* View Profile Button */}
-                    <Button className="border h-12 w-1/2 text-sm font-medium text-gray-700 hover:text-gray-900 bg-gray-100 rounded-lg hover:bg-gray-200 flex items-center gap-2 transition-colors duration-200">
+                    <Button className="border md:h-12 w-1/2 text-sm font-medium text-gray-700 hover:text-gray-900 bg-gray-100 rounded-lg hover:bg-gray-200 flex items-center gap-2 transition-colors duration-200">
                         <ExternalLink className="w-4 h-4" />
                         View Profile
                     </Button>
 
                     {/* Book Button */}
-                    <Button className="h-12 w-1/2 gradient-color">Book</Button>
+                    <Button className="md:h-12 w-1/2 gradient-color">Book</Button>
                 </div>
 
                 {/* Check Availability Link */}
                 <Link href="#" className="flex justify-center gap-1 cursor-pointer underline">
                     <Calendar className="w-4 text-primary" />
                     <span className="text-primary">Check availability</span>
-                    <ChevronRight className="text-primary w-5 -ml-1"/>
+                    <ChevronRight className="text-primary w-5 -ml-1" />
                 </Link>
             </div>
 
-            
+
         </div>
     );
 };
--- src/app/(root)/instructors/components/InstructorGrid.tsx
+++ src/app/(root)/instructors/components/InstructorGrid.tsx
@@ -73,6 +73,50 @@ const instructors = [
         totalRatings: 100,
         totalLessons: 100
     },
+    {
+        id: 7,
+        name: "John Doe",
+        image: cardImg,
+        rating: 4.8,
+        experience: "5 years",
+        pricePerHour: 50,
+        skills: "Auto",
+        totalRatings: 80,
+        totalLessons: 100
+    },
+    {
+        id: 8,
+        name: "Jane Smith",
+        image: cardImg,
+        rating: 4.7,
+        experience: "3 years",
+        pricePerHour: 55,
+        skills: "Manual",
+        totalRatings: 100,
+        totalLessons: 100
+    },
+    {
+        id: 9,
+        name: "John Doe",
+        image: cardImg,
+        rating: 4.8,
+        experience: "5 years",
+        pricePerHour: 50,
+        skills: "Auto",
+        totalRatings: 80,
+        totalLessons: 100
+    },
+    {
+        id: 10,
+        name: "Jane Smith",
+        image: cardImg,
+        rating: 4.7,
+        experience: "3 years",
+        pricePerHour: 55,
+        skills: "Manual",
+        totalRatings: 100,
+        totalLessons: 100
+    },
 ];
 
 const InstructorGrid: FC = () => {
@@ -81,7 +125,7 @@ const InstructorGrid: FC = () => {
     const [totalPages, setTotalPages] = useState(4);
     return (
         <>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10">
                 {
                     instructors.map((instructor) => (
                         <InstructorCard key={instructor.id} instructor={instructor} />
