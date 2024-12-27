8511a59a43a4de30e8473453203a5351008dd561
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2024-12-27 18:06:01 +0600

    feat: add login step form on register step

---
+++ src/app/(root)/instructors/booking/components/register-step/LoginStepForm.tsx
@@ -0,0 +1,109 @@
+"use client"
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { toast } from '@/hooks/use-toast';
+import { useLoginUserMutation } from '@/redux/api/authApi/authApi';
+import { useAppDispatch } from '@/redux/hook';
+import { saveUser } from '@/redux/slices/authSlice/authSlice';
+import { ILoginInputs } from '@/types/auth';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { FC } from 'react';
+import { useForm } from 'react-hook-form';
+
+
+
+const LoginFormStep: FC = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm<ILoginInputs>();
+    const [loginUser, { isLoading: isLoginLoading }] = useLoginUserMutation();
+
+    const dispatch = useAppDispatch();
+
+    const urlSearchParams = useSearchParams();
+    const router = useRouter();
+
+    const handleLogin = (data: ILoginInputs) => {
+        loginUser(data).unwrap().then((res) => {
+            toast({
+                message: res.message
+            });
+            dispatch(saveUser({ user: res.data, isAuthenticate: true, isLoading: false }));
+            router.push('?step=payment')
+        }).catch((err) => {
+            toast({
+                success: false,
+                message: err.data.message || "Something went wrong"
+            });
+        })
+    }
+
+
+    // handler for navigating with exisiting query
+    const handleNavigate = () => {
+        const searchParams = new URLSearchParams(urlSearchParams);
+        searchParams.set('step', 'register');
+        router.replace(`?${searchParams.toString()}`);
+    }
+
+
+    return (
+        <form
+            onSubmit={handleSubmit(handleLogin)}
+            className='bg-white p-6 rounded-lg shadow-sm border border-gray-200'
+        >
+            <h2 className="text-xl font-semibold mb-6 text-left">Learner Login</h2>
+
+            <div className='w-full mt-7'>
+                <div className='flex flex-col gap-5'>
+                    {/* Email */}
+                    <div>
+                        <label htmlFor="email" className='font-semibold text-secondary'>Email/Phone</label>
+                        <Input
+                            {...register('emailOrPhone', {
+                                required: "Email is required",
+                                pattern: {
+                                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                    message: "Invalid email address"
+                                }
+                            })}
+                            type="text" id='emailOrPhone' placeholder="Enter your email or phone" className='xl:h-12 mt-1'
+                        />
+                        {errors?.emailOrPhone && <p className='text-red-500 text-sm mt-1'>{errors?.emailOrPhone?.message}</p>}
+                    </div>
+
+                    {/* Password */}
+                    <div>
+                        <label htmlFor="password" className='font-semibold text-secondary'>Password</label>
+                        <Input
+                            {...register('password', {
+                                minLength: {
+                                    message: "Password must be at least 6 characters", value: 6
+                                },
+                                required: "Password is required"
+                            })
+                            }
+                            type="password" id='password' placeholder="Enter your password" className='xl:h-12 mt-1'
+                        />
+                        {errors?.password && <p className='text-red-500 text-sm mt-1'>{errors?.password?.message}</p>}
+                    </div>
+                </div>
+
+                {/* <p className='text-right mt-2'>
+                    <Link href="#" className='text-secondary hover:underline font-semibold'>Forgot password?</Link>
+                </p> */}
+            </div>
+
+            {/* Submit */}
+            <div className='text-center'>
+                <Button className='w-full mx-auto mt-6 gradient-color h-12' disabled={isLoginLoading}>Login</Button>
+            </div>
+
+            <p className='mt-5'>Don&apos;t have an account? <span onClick={handleNavigate} className='text-blue-500 hover:underline font-semibold cursor-pointer'>
+                    Register Here
+                </span>
+            </p>
+        </form>
+    );
+};
+
+export default LoginFormStep;
