ae7685e6101eee4a43f9c48b304bb46cc04d403a
hossainarif37 <hossainarif4937@gmail.com>
2024-12-27 12:08:59 +0600

    feat: Add SharedProfile Component for Profile Settings Page
    - Introduced new `SharedProfile` component to manage profile settings page.
    - Integrated reusable `PersonalInfoFields` component for form handling.
    - Added password visibility toggle for password fields.
    - Implemented form submission logic with form validation.
    - Enhanced UI with a 'Update' button for saving changes.
    - Updated `PersonalInfoForm` to leverage the new `PersonalInfoFields` for better reusability and maintainability.

--- src/app/dashboard/profile-settings/components/shared-profile/SharedProfile.tsx
+++ src/app/dashboard/profile-settings/components/shared-profile/SharedProfile.tsx
@@ -1,10 +1,53 @@
-import { FC } from 'react';
+"use client"
+
+import { IPersonalInfo } from '@/app/(auth)/instructor-registration/components/InstructorRegistration';
+import PersonalInfoFields from '@/components/shared/forms/PersonalInfoFields';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { genderOptions } from '@/constant/gender';
+import { IRegisterInputs } from '@/types/auth';
+import { Eye, EyeClosed, EyeOff } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { FC, useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
 
 const SharedProfile: FC = () => {
+    const { register, handleSubmit, formState: { errors }, control, watch } = useForm<IPersonalInfo>();
+    const [confirmPasswordError, setConfirmPasswordError] = useState<string>("");
+    const [passwordVisible, setPasswordVisible] = useState(false);
+    const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
+    // Watch password fields
+    // const password = watch('password');
+    // const confirmPassword = watch('confirmPassword');
+    // const router = useRouter();
+
+    const onSubmit = (data: IPersonalInfo) => {
+        console.log(data);
+    }
+    const handlePasswordToggle = (field: string) => {
+        if (field === 'password') {
+            setPasswordVisible((prev) => !prev);
+        } else if (field === 'confirm-password') {
+            setConfirmPasswordVisible((prev) => !prev);
+        }
+    }
+
     return (
-        <div>
-            SharedProfile
-        </div>
+        <form
+            onSubmit={handleSubmit(onSubmit)}
+            className='w-full md:w-[500px] lg:w-[750px] mx-auto p-5 md:p-10 flex flex-col items-start md:shadow-lg bg-light md:rounded-lg md:border'
+        >
+            <h1 className='text-2xl md:text-3xl font-bold text-secondary'>Personal Details</h1>
+
+            <PersonalInfoFields
+                register={register}
+                errors={errors}
+                control={control}
+            />
+
+            <Button className='w-full mt-7 gradient-color h-12'>Update</Button>
+        </form >
     );
 };
 
