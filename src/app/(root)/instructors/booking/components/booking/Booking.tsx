433cd0dad1b0c7382b4b4855198555b639492cf0
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2024-12-27 17:50:57 +0600

    add: package selection step with package card

--- src/app/(root)/instructors/booking/components/booking/Booking.tsx
+++ src/app/(root)/instructors/booking/components/booking/Booking.tsx
@@ -1,16 +1,24 @@
 "use client"
+import { useSearchParams } from 'next/navigation';
 import { FC } from 'react';
+import PacakageSelectionStep from '../package-selection-step/PacakageSelectionStep';
 import BookingInfo from '../booking-info/BookingInfo';
-import { BookingProvider } from '@/providers/BookingProvider';
 import InstructorInfo from '../instructor-info/InstructorInfo';
+import { BookingProvider } from '@/providers/BookingProvider';
 
 const Booking: FC = () => {
+    const urlSearchParams = useSearchParams();
+    const step = urlSearchParams.get('step');
+
+    const pacakageSelectionStep = step === 'package-selection';
 
     return (
         <BookingProvider>
             <div className='py-8 grid grid-cols-6 gap-6'>
                 <div className='col-span-4'>
-
+                    {
+                        pacakageSelectionStep && <PacakageSelectionStep />
+                    }
                 </div>
                 <div className='col-span-2 space-y-6'>
                     <BookingInfo />
