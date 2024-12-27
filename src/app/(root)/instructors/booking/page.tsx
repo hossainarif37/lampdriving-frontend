33ea378d21699b3be9303f6d7ad9c66286b9b282
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2024-12-27 16:56:29 +0600

    feat: update BookingPage to include Booking comp

--- src/app/(root)/instructors/booking/page.tsx
+++ src/app/(root)/instructors/booking/page.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react';
-import BookingSteps from './components/BookingSteps/BookingSteps';
+import BookingSteps from './components/booking-steps/BookingSteps';
+import Booking from './components/booking/Booking';
+
 
 const BookingPage: FC = () => {
-
     return (
-        <div className='wrapper'>
+        <div className='wrapper py-8'>
             <BookingSteps />
+            <Booking />
         </div>
     );
 };
