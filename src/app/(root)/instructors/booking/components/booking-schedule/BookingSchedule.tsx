120fdb1e91f035d2e413d5da9bb7451c82b17981
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-01-01 01:37:03 +0600

    refactor: add fixed height and scrollbar on booking schedule

--- src/app/(root)/instructors/booking/components/booking-schedule/BookingSchedule.tsx
+++ src/app/(root)/instructors/booking/components/booking-schedule/BookingSchedule.tsx
@@ -12,27 +12,29 @@ const BookingSchedule: FC = () => {
                 schedules &&
                 <div className="bg-white p-6 rounded-lg shadow-sm border border-gray-200">
                     <h2 className="text-lg font-semibold mb-4">Booking Schedule</h2>
-                    {
-                        schedules.map((schedule, index) => (
-                            <div key={index} className="space-y-3 border-y py-2">
-                                <div className="flex items-center justify-between">
-                                    <p className="flex items-start gap-2">
-                                        <CalendarIcon className="size-5 text-primary mt-0.5" />
-                                        {format(new Date(schedule.date), 'MMMM dd, yyyy')} at {schedule.time[0]}
-                                    </p>
+                    <div className='h-[234px] overflow-y-auto thin-scrollbar'>
+                        {
+                            schedules.map((schedule, index) => (
+                                <div key={index} className="space-y-3 border-y py-2">
+                                    <div className="flex items-center justify-between">
+                                        <p className="flex items-start gap-2">
+                                            <CalendarIcon className="size-5 text-primary mt-0.5" />
+                                            {format(new Date(schedule.date), 'MMMM dd, yyyy')} at {schedule.time[0]}
+                                        </p>
+                                    </div>
+                                    <div className='flex items-center justify-between'>
+                                        <p className='flex items-center gap-2'>
+                                            <Timer className='size-5 text-primary' /> {schedule.duration == 1.5 ? '1.5-Hours' : `${schedule.duration}-Hours`}
+                                        </p>
+                                        <p className="flex items-center gap-2">
+                                            <MapPinIcon className="size-5 text-primary mt-0.5" />
+                                            {schedule.pickupAddress.suburb}
+                                        </p>
+                                    </div>
                                 </div>
-                                <div className='flex items-center justify-between'>
-                                    <p className='flex items-center gap-2'>
-                                        <Timer className='size-5 text-primary' /> {schedule.duration == 1.5 ? '1.5-Hours' : `${schedule.duration}-Hours`}
-                                    </p>
-                                    <p className="flex items-center gap-2">
-                                        <MapPinIcon className="size-5 text-primary mt-0.5" />
-                                        {schedule.pickupAddress.suburb}
-                                    </p>
-                                </div>
-                            </div>
-                        ))
-                    }
+                            ))
+                        }
+                    </div>
                 </div>
             }
         </>
