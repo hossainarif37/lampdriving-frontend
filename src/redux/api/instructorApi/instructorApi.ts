fd69eb01a7f1c85ca5a5ae1fc1a3e9691304a69d
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-01-01 10:32:04 +0600

    add: getInstructorAvailability endpoint

--- src/redux/api/instructorApi/instructorApi.ts
+++ src/redux/api/instructorApi/instructorApi.ts
@@ -6,9 +6,12 @@ const instructorApi = baseApi.injectEndpoints({
     endpoints: (builder) => ({
         getAInstructor: builder.query<IResponseWithData<IInstructor>, { username: string }>({
             query: ({ username }) => `/instructor/${username}`
+        }),
+        getInstructorAvailability: builder.query<IResponseWithData<{ schedules: [{ date: string, time: [string] }] }>, { id: string }>({
+            query: ({ id }) => `/instructor/availability/${id}`
         })
     })
 })
 
 
-export const { useGetAInstructorQuery } = instructorApi;
+export const { useGetAInstructorQuery, useGetInstructorAvailabilityQuery } = instructorApi;
