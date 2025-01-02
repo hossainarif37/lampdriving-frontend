dc41d3f693135443cc333739e4cb5ba173696d81
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-01-02 20:14:58 +0600

    chore: update layout padding

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -5,18 +5,15 @@ import { FC } from 'react';
 
 const DashboardLayout: FC<{ children: ReactNode }> = ({ children }) => {
     return (
-        <div className='flex flex-col lg:flex-row bg-gray-100 '>
-            <div className='lg:min-h-screen lg:w-80 relative bg-white'>
+        <div className='flex flex-col lg:flex-row bg-white'>
+            <div className='lg:min-h-screen lg:w-80 relative '>
                 <Sidebar />
             </div>
             <div className='flex-1 min-h-screen'>
                 <DashboardNav />
-                <div className='pt-[80px] pb-3 lg:py-3 pl-6 px-3'>
+                <div className='pt-[85px] pb-3 lg:py-6 px-3 lg:px-6 bg-gray-100 rounded-tl-md'>
                     {children}
                 </div>
-                {/* <div className="min-h-screen"></div> */}
-                {/* <div className="min-h-screen"></div>
-                <div className="min-h-screen"></div> */}
             </div>
         </div>
     );
