685b1f1107446ac2955e129a4b62ebae248a1098
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-01-04 04:22:10 +0600

    fix: adjust padding and width for footer search components

--- src/components/layouts/footer/FooterSearch.tsx
+++ src/components/layouts/footer/FooterSearch.tsx
@@ -23,7 +23,7 @@ const FooterSearchBoxDesign: FC = () => {
         setCarType('auto');
     }
     return (
-        <div className="lg:max-w-5xl md:max-w-3xl h-48  md:h-40 flex flex-col md:flex-row items-center justify-center md:gap-4 gap-6 bg-[#212139] w-full md:rounded-md rounded-lg absolute -top-20 left-1/2 right-1/2 -translate-x-1/2 md:px-20 px-6">
+        <div className="lg:max-w-5xl md:max-w-3xl h-48 md:h-40 flex flex-col md:flex-row items-center justify-center md:gap-4 gap-6 bg-[#212139] w-full md:rounded-md rounded-lg absolute -top-20 left-1/2 right-1/2 -translate-x-1/2 md:px-20 px-6">
 
             <div className='md:w-10/12 flex flex-col lg:flex-row items-center justify-between gap-2'>
 
@@ -31,7 +31,7 @@ const FooterSearchBoxDesign: FC = () => {
                 <div className="w-full font-semibold text-light text-center flex gap-1 border p-1 rounded-md bg-light">
                     <Button
                         onClick={() => setCarType('auto')}
-                        className={`w-1/2 h-[40px] hover:bg-gray-200 flex justify-center items-center rounded-md ${carType === 'auto'
+                        className={`w-1/2 h-[40px] px-6 lg:px-7 hover:bg-gray-200 flex justify-center items-center rounded-md ${carType === 'auto'
                             ? 'gradient-color text-light'
                             : 'bg-gray-200 text-secondary'}`}
                     >
@@ -39,7 +39,7 @@ const FooterSearchBoxDesign: FC = () => {
                     </Button>
                     <Button
                         onClick={() => setCarType('manual')}
-                        className={`w-1/2 h-[40px] hover:bg-gray-200 flex justify-center items-center rounded-md ${carType === 'manual'
+                        className={`w-1/2 h-[40px] px-6 lg:px-7 hover:bg-gray-200 flex justify-center items-center rounded-md ${carType === 'manual'
                             ? 'gradient-color text-light'
                             : 'bg-gray-200 text-secondary'}`}
                     >
@@ -58,12 +58,12 @@ const FooterSearchBoxDesign: FC = () => {
                                 value={selectedSuburb}
                                 onChange={(e) => setSelectedSuburb(e.target.value)}
                                 readOnly={selectedSuburb ? true : false}
-                                className="md:flex-1 px-4 py-3 rounded-md text-secondary focus:outline-none placeholder:text-accent w-[300px] md:w-[380px] lg:w-[340px]"
+                                className="md:flex-1 px-4 py-3 rounded-md text-secondary focus:outline-none placeholder:text-accent w-[300px] md:w-[380px] lg:w-[280px]"
                                 type="text"
                                 placeholder="Enter your suburb"
                             />
                         </PopoverTrigger>
-                        <PopoverContent className="w-[300px] md:w-[380px] lg:w-[340px] p-2">
+                        <PopoverContent className="w-[300px] md:w-[380px] lg:w-[280px] p-2">
                             <Command>
                                 <CommandInput placeholder="Enter your suburb" />
                                 <CommandList>
@@ -83,7 +83,7 @@ const FooterSearchBoxDesign: FC = () => {
                             </Command>
                         </PopoverContent>
                     </Popover>
-                    <Button onClick={handleSearch} className="py-3 gradient-color text-light rounded-md text-base w-full md:w-auto">
+                    <Button onClick={handleSearch} className="py-3 gradient-color text-light rounded-md text-base w-full md:w-auto px-6 lg:px-7">
                         Search
                     </Button>
                 </div>
