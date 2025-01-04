8ccb78d5bff6e9945e34a9d7f33ea0a04581010e
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-01-04 06:25:42 +0600

    feat: add getMyBookings endpoint

--- src/redux/api/bookingApi/bookingApi.ts
+++ src/redux/api/bookingApi/bookingApi.ts
@@ -23,6 +23,9 @@ const bookingApi = baseApi.injectEndpoints({
         getAllBookings: builder.query<IResponseWithPaginationData<IBooking[]>, IGetAllBookingsQuery>({
             query: ({ status, searchKey, limit, page }) => `/booking/all?status=${status}&populate=instructor.user,learner.user${searchKey && `&searchKey=${searchKey}`}&limit=${limit}&page=${page}`, providesTags: ["booking"]
         }),
+        getMyBookings: builder.query<IResponseWithPaginationData<IBooking[]>, IGetAllBookingsQuery>({
+            query: ({ status, searchKey, limit, page }) => `/booking/my?status=${status}&populate=instructor.user,learner.user${searchKey && `&searchKey=${searchKey}`}&limit=${limit}&page=${page}`, providesTags: ["booking"]
+        }),
         updateBookingStatus: builder.mutation<IResponseBase, { id: string, status: "pending" | "accepted" | "completed" | "cancelled" }>({
             query: ({ id, status }) => ({
                 url: `/booking/status/${id}`,
@@ -35,4 +38,4 @@ const bookingApi = baseApi.injectEndpoints({
 })
 
 
-export const { useCreateABookingMutation, useGetAllBookingsQuery, useUpdateBookingStatusMutation } = bookingApi
+export const { useCreateABookingMutation, useGetAllBookingsQuery, useUpdateBookingStatusMutation, useGetMyBookingsQuery } = bookingApi
