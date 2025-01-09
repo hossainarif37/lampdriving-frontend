e8f4ba86cc78e0f3cae903e427cb1a9dde040b4c
Basirul-Hridoy <basirulislam.hridoy05@gmail.com>
2025-01-09 22:33:35 +0600

    code updated

--- src/app/(root)/components/shared/faq-accordion/FaqAccordion.tsx
+++ src/app/(root)/components/shared/faq-accordion/FaqAccordion.tsx
@@ -1,26 +1,22 @@
 import { FC } from 'react';
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion"
+import { AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion"
 interface IFaqProps {
     faqInfo: {
         title: string;
         description: string;
         val: string;
     };
-    title?: string;
-    description?: string;
 }
 const FaqAccordion: FC<IFaqProps> = ({ faqInfo }) => {
     const { title, description, val } = faqInfo;
     return (
         <div>
-            {/* <Accordion type="single" collapsible className="w-full"> */}
             <AccordionItem value={val} className=''>
                 <AccordionTrigger className='hover:no-underline text-primary'>{title}</AccordionTrigger>
                     <AccordionContent className='text-accent'>
                     {description}
                     </AccordionContent>
-                </AccordionItem>
-            {/* </Accordion> */}
+            </AccordionItem>
         </div>
     );
 };
