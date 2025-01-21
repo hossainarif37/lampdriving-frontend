164fbb7b2d469c68ccc8f847188ca0fed22edd87
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-01-21 15:11:13 +0600

    feat: add instructor schedules query and schedule type definition

--- src/redux/api/scheduleApi/scheduleApi.ts
+++ src/redux/api/scheduleApi/scheduleApi.ts
@@ -1,14 +1,18 @@
 import { IResponseWithData } from "@/types/response";
 import baseApi from "../baseApi";
+import { ISchedule } from "@/types/schedule";
 
 const scheduleApi = baseApi.injectEndpoints({
     overrideExisting: true,
     endpoints: (builder) => ({
         getInstructorAvailability: builder.query<IResponseWithData<{ schedules: [{ date: string, time: [string] }] }>, { id: string }>({
             query: ({ id }) => `/schedule/availability/${id}`
+        }),
+        getInstructorsSchedules: builder.query<IResponseWithData<ISchedule>, { id: string }>({
+            query: ({ id }) => `/schedule/instructor/${id}`
         })
     })
 })
 
 
-export const { useGetInstructorAvailabilityQuery } = scheduleApi; 
+export const { useGetInstructorAvailabilityQuery, useGetInstructorsSchedulesQuery } = scheduleApi; 
