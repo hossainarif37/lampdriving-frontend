20232a9000cab1c9acccfd96ed010a0dac088f93
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-01-21 14:51:08 +0600

    feat: add manage schedules table

---
+++ src/app/dashboard/instructor/manage-schedules/components/ManageSchedulesActions.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const ManageSchedulesActions: FC<{ id: string }> = () => {
+    return (
+        <div>
+            ManageSchedulesActions
+        </div>
+    );
+};
+
+export default ManageSchedulesActions;
