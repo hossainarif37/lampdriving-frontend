2984fe5b95045d8d1697aa46311b2635c3b81f7e
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-01-21 10:52:19 +0600

    Enhance OngoingBookingsTable to display payment details and improve loading component styling

--- src/components/shared/Loading.tsx
+++ src/components/shared/Loading.tsx
@@ -1,10 +1,8 @@
 import { cn } from "@/lib/utils";
-import { LoaderCircle } from "lucide-react";
 
-
-const Loading = ({ className }: { className?: string }) => {
+const Loading = ({ className, parentClassName }: { className?: string, parentClassName?: string }) => {
     return (
-        <div className="flex items-center justify-center min-h-screen">
+        <div className={cn("flex items-center justify-center min-h-screen", parentClassName)}>
             <div className={cn(`animate-spin w-10 rounded-full h-10 border-t-2 border-r-2 border-t-primary border-r-primary`, className)} />
         </div>
     );
