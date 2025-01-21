db2b031c1208a6b1bfa3ee708159d326de6e346d
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-01-21 19:12:00 +0600

    feat: add rescheduling functionality with location and time selection components

--- src/app/dashboard/instructor/manage-schedules/components/reschedule-a-schedule/RescheduleASchedule.tsx
+++ src/app/dashboard/instructor/manage-schedules/components/reschedule-a-schedule/RescheduleASchedule.tsx
@@ -4,14 +4,13 @@ import { Clock, XCircle } from 'lucide-react';
 import { Dispatch, FC, SetStateAction, useEffect, useState } from 'react';
 
 import { Button } from '@/components/ui/button';
-import { format } from 'date-fns';
-import Link from 'next/link';
+import { format, set } from 'date-fns';
 import { IWorkingHour } from '@/types/instructor';
 import { useGetInstructorAvailabilityQuery } from '@/redux/api/scheduleApi/scheduleApi';
-import ScheduleCalender from '@/app/(root)/instructors/booking/components/schedule-step/ScheduleCalender';
-import ScheduleTimeSlots from '@/app/(root)/instructors/booking/components/schedule-step/ScheduleTimeSlots';
 import { useGetAInstructorQuery } from '@/redux/api/instructorApi/instructorApi';
 import { IAddress } from '@/types/user';
+import RescheduleTime from './RescheduleTime';
+import RescheduleLocation from './RescheduleLocation';
 
 interface IRescheduleAScheduleProps {
     id: string;
@@ -21,9 +20,11 @@ interface IRescheduleAScheduleProps {
     duration: number;
     pickupAddress: IAddress;
     dropOffAddress?: IAddress;
+    type: "lesson" | "test" | "mock-test";
 }
 
-const RescheduleASchedule: FC<IRescheduleAScheduleProps> = ({ id, username, showAvailability, setShowAvailability, duration }) => {
+const RescheduleASchedule: FC<IRescheduleAScheduleProps> = ({ id, username, showAvailability, setShowAvailability, duration, type }) => {
+    const [step, setStep] = useState<"time" | "location">("time");
     const [selectedDate, setSelectedDate] = useState<Date | null>(null);
     const [selectedTime, setSelectedTime] = useState<string[]>([]);
     const [bookedTimeSlots, setBookedTimeSlots] = useState<string[]>([]);
@@ -31,6 +32,18 @@ const RescheduleASchedule: FC<IRescheduleAScheduleProps> = ({ id, username, show
     const [scheduleTimeSlots, setScheduleTimeSlots] = useState<string[]>([]);
     const { data: instructorData } = useGetAInstructorQuery({ username });
     const { data } = useGetInstructorAvailabilityQuery({ id: instructorData?.data._id || '' });
+    const [pickupLocation, setPickupLocation] = useState<IAddress>({ address: '', suburb: '' });
+    const [pickupLocationError, setPickupLocationError] = useState<{ address: boolean, suburb: boolean }>({ address: false, suburb: false });
+    const [dropOffLocation, setDropOffLocation] = useState<IAddress>({ address: '', suburb: '' });
+    const [dropOffLocationError, setDropOffLocationError] = useState<{ address: boolean, suburb: boolean }>({ address: false, suburb: false });
+
+
+    const handleSubmit = () => {
+        if (step === "time" && selectedDate && selectedTime.length > 0) {
+            setStep("location");
+        }
+    }
+
     useEffect(() => {
         if (!selectedDate) {
             setBookedTimeSlots([]);
@@ -64,7 +77,7 @@ const RescheduleASchedule: FC<IRescheduleAScheduleProps> = ({ id, username, show
             <DialogContent className='max-w-3xl py-0 px-0 space-y-0 gap-0'>
                 <DialogHeader>
                     <DialogTitle className='text-2xl font-semibold px-4 pt-3 pb-1 text-center'>
-                        <span className='text-primary'>Reschedule a Schedule {username}</span>
+                        <span className='text-primary'>Reschedule a Schedule</span>
                     </DialogTitle>
                 </DialogHeader>
                 <div className="flex items-center justify-center pb-3 gap-6 mt-4 text-sm text-gray-600">
@@ -77,48 +90,58 @@ const RescheduleASchedule: FC<IRescheduleAScheduleProps> = ({ id, username, show
                         <span>Closed / Booked</span>
                     </div>
                 </div>
-                <div className='grid grid-cols-2 text-black border-y'>
-                    <div>
-                        <ScheduleCalender
-                            availableScheduleHours={1}
-                            schedules={[]}
-                            bookedSchedules={data?.data.schedules || []}
-                            workingHours={instructorData?.data.workingHour as IWorkingHour}
-                            classname='border-y-0 border-l-0 shadow-none border-r rounded-none'
-                            selectedDate={selectedDate}
-                            onSelectDate={setSelectedDate}
-                        />
-                    </div>
-                    <div>
-                        <ScheduleTimeSlots
-                            scheduleTimeSlots={scheduleTimeSlots}
-                            setScheduleTimeSlots={setScheduleTimeSlots}
-                            workingHour={workingHour}
-                            classname='border-none shadow-none'
-                            bookedTimeSlots={bookedTimeSlots}
-                            selectedDuration={duration}
-                            selectedTime={selectedTime}
-                            onSelectTime={setSelectedTime}
-                            selectedDate={selectedDate}
-                            availableScheduleHours={1}
-                        />
-                    </div>
+                <div className='grid grid-cols-2 text-black border-y min-h-[372px]'>
+                    {
+                        step === "time" ?
+                            <RescheduleTime
+                                selectedDate={selectedDate}
+                                setSelectedDate={setSelectedDate}
+                                scheduleTimeSlots={scheduleTimeSlots}
+                                setScheduleTimeSlots={setScheduleTimeSlots}
+                                workingHour={workingHour}
+                                duration={duration}
+                                selectedTime={selectedTime}
+                                bookedTimeSlots={bookedTimeSlots}
+                                data={data}
+                                instructorData={instructorData}
+                                setSelectedTime={setSelectedTime}
+                            />
+                            :
+                            <RescheduleLocation
+                                type={type}
+                                setPickupLocation={setPickupLocation}
+                                dropOffLocation={dropOffLocation}
+                                dropOffLocationError={dropOffLocationError}
+                                pickupLocation={pickupLocation}
+                                pickupLocationError={pickupLocationError}
+                                setDropOffLocation={dropOffLocation}
+                            />
+                    }
                 </div>
                 <DialogFooter className='pt-4 bg-gray-50'>
                     <div className="flex items-center justify-end gap-4 pb-4 px-4">
-                        <DialogClose asChild>
-                            <Button
-                                variant={"outline"}
-                                className=""
-                            >
-                                Cancel
-                            </Button>
-                        </DialogClose>
-                        <Link href={`/instructors/booking?instructor=${username}&step=package-selection`}>
-                            <Button className='bg-primary' size="lg">
-                                Continue
-                            </Button>
-                        </Link>
+                        {
+                            step == "time" ?
+                                <DialogClose asChild>
+                                    <Button
+                                        variant={"outline"}
+                                        className=""
+                                    >
+                                        Cancel
+                                    </Button>
+                                </DialogClose>
+                                :
+                                <Button
+                                    variant={"outline"}
+                                    className=""
+                                    onClick={() => setStep("time")}
+                                >
+                                    Back
+                                </Button>
+                        }
+                        <Button onClick={handleSubmit} disabled={!selectedDate || selectedTime.length === 0} className='bg-primary' size="lg">
+                            Continue
+                        </Button>
                     </div>
                 </DialogFooter>
             </DialogContent>
