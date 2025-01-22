b8d7f57407ffbff493f0c6725d4afaa8bce4be4f
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-01-22 08:30:52 +0600

    feat: enhance ScheduleTimeSlots component with slotContainerClassname prop and update ManageAvailability layout

--- src/app/dashboard/instructor/manage-availability/page.tsx
+++ src/app/dashboard/instructor/manage-availability/page.tsx
@@ -8,11 +8,13 @@ const ManageAvailabilityPage: FC = () => {
             <div className='py-4 px-5 flex items-center justify-between'>
                 <h2 className='font-semibold text-2xl'>Manage Availability</h2>
             </div>
-            <div className='grid grid-cols-12 gap-6'>
+            <div className='grid grid-cols-12 gap-6 px-5 py-4'>
                 <div className='col-span-8'>
                     <ManageAvailability />
                 </div>
                 <div className='col-span-4'>
+                </div>
+                <div className='col-span-12'>
                     <ManageWorkingHours />
                 </div>
             </div>
