9229ecd68168689fa7d12cc18c129b0c35e09f3f
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-01-23 11:41:30 +0600

    feat: add ViewDetailsDialog and ViewDetailsDialogBtn components for enhanced detail viewing functionality

---
+++ src/app/dashboard/components/shared/view-details/ViewDetailsDialog.tsx
@@ -0,0 +1,45 @@
+"use client"
+import { Dialog, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
+import { Dispatch, FC, ReactNode, SetStateAction, useState } from 'react';
+import { Button } from '@/components/ui/button';
+
+
+interface IViewDetailsDialogProps {
+    children: ReactNode;
+    showAvailability: boolean;
+    setShowAvailability: Dispatch<SetStateAction<boolean>>;
+    title: string;
+}
+
+
+const ViewDetailsDialog: FC<IViewDetailsDialogProps> = ({ children, showAvailability, setShowAvailability, title }) => {
+    return (
+        <Dialog open={showAvailability} onOpenChange={setShowAvailability}>
+            <DialogTrigger asChild>
+                <Button variant={"ghost"} className='h-[36px] py-0 font-normal capitalize text-start justify-start px-2'>View Details</Button>
+            </DialogTrigger>
+            <DialogContent className='max-w-3xl py-0 px-0 space-y-0 gap-0'>
+                <DialogHeader>
+                    <DialogTitle className='text-2xl font-semibold px-4 pt-3 pb-1 text-center'>
+                        <span className='text-primary'>{title}</span>
+                    </DialogTitle>
+                </DialogHeader>
+                {children}
+                <DialogFooter className='pt-4 bg-gray-50'>
+                    <div className="flex items-center justify-end gap-4 pb-4 px-4">
+                        <DialogClose asChild>
+                            <Button
+                                variant={"outline"}
+                                className=""
+                            >
+                                Close
+                            </Button>
+                        </DialogClose>
+                    </div>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog >
+    );
+};
+
+export default ViewDetailsDialog;
---
+++ src/app/dashboard/components/shared/view-details/ViewDetailsDialogBtn.tsx
@@ -0,0 +1,43 @@
+"use client"
+import { Button } from '@/components/ui/button';
+import dynamic from 'next/dynamic';
+import { FC, ReactNode, useState } from 'react';
+
+const ViewDetailsDialog = dynamic(
+    () => import('./ViewDetailsDialog'),
+    {
+        loading: () => (
+            <Button variant={"ghost"} className='h-[36px] py-0 font-normal capitalize text-start justify-start px-2 w-full'>View Details</Button>
+        ),
+        ssr: false
+    }
+);
+
+interface IViewDetailsDialogBtnProps {
+    children: ReactNode;
+    title: string;
+}
+
+
+const ViewDetailsDialogBtn: FC<IViewDetailsDialogBtnProps> = ({ children, title }) => {
+    const [showAvailability, setShowAvailability] = useState(false);
+
+    return (
+        <div className="text-center flex items-center justify-center w-full">
+            {showAvailability ? (
+                <ViewDetailsDialog
+                    title={title}
+                    showAvailability={showAvailability}
+                    setShowAvailability={setShowAvailability}
+                    children={children}
+                />
+            ) : (
+                <Button
+                    onClick={() => setShowAvailability(true)}
+                    variant={"ghost"} className='h-[36px] py-0 font-normal capitalize text-start justify-start px-2 w-full'>View Details</Button>
+            )}
+        </div>
+    );
+};
+
+export default ViewDetailsDialogBtn;
