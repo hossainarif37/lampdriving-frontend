f4993e962986e44c67933bd4512f5c301476e49d
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-01-24 12:53:07 +0600

    feat(learner-details): add LearnerDetails component and skeleton for displaying learner information

---
+++ src/app/dashboard/admin/manage-learners/components/learner-details/LearnerDetailsSkeleton.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const LearnerDetailsSkeleton: FC = () => {
+    return (
+        <div>
+            LearnerDetailsSkeleton
+        </div>
+    );
+};
+
+export default LearnerDetailsSkeleton;
