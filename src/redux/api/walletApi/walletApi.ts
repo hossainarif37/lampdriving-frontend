7218a25f9b9aa00efe3570875d16a56401c63070
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-01-24 16:14:43 +0600

    feat(wallet-api): add addBankAccount mutation to handle bank account updates

--- src/redux/api/walletApi/walletApi.ts
+++ src/redux/api/walletApi/walletApi.ts
@@ -1,4 +1,4 @@
-import { IResponseWithData } from "@/types/response";
+import { IResponseBase, IResponseWithData } from "@/types/response";
 import baseApi from "../baseApi";
 import { IWallet } from "@/types/wallet";
 
@@ -6,19 +6,26 @@ const walletApi = baseApi.injectEndpoints({
     overrideExisting: true,
     endpoints: (builder) => ({
         getInstructorWallet: builder.query<IResponseWithData<IWallet>, { instructorId: string }>({
-            query: ({ instructorId }) => `/wallet/instructor/${instructorId}`
+            query: ({ instructorId }) => `/wallet/instructor/${instructorId}`,
+            providesTags: ["wallet"]
         }),
-
         getAllWallet: builder.query({
             query: () => `/wallet/all?populate=instructor,instructor.user&balance.currentBalance[$gt]=1&instructorFields=user,completedLessons`,
             providesTags: ["wallet"]
         }),
-
         getAdminWallet: builder.query({
             query: () => `/wallet/admin`,
             providesTags: ["wallet"]
+        }),
+        addBankAccount: builder.mutation<IResponseBase, { walletId: string, payId: string }>({
+            query: ({ walletId, payId }) => ({
+                url: `/wallet/bank-account/${walletId}`,
+                method: "PATCH",
+                body: { payId }
+            }),
+            invalidatesTags: ["wallet"]
         })
     })
 })
 
-export const { useGetInstructorWalletQuery, useGetAllWalletQuery, useGetAdminWalletQuery } = walletApi;
+export const { useGetInstructorWalletQuery, useGetAllWalletQuery, useGetAdminWalletQuery, useAddBankAccountMutation } = walletApi;
