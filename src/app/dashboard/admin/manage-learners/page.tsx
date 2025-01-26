1b0cbb3a342dc703891a4d6da0942da121017575
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-01-26 09:56:46 +0600

    feat(ManageLearners): add user status filter component and integrate with learners table query

--- src/app/dashboard/admin/manage-learners/page.tsx
+++ src/app/dashboard/admin/manage-learners/page.tsx
@@ -1,13 +1,18 @@
 import { FC } from 'react';
 import LearnersTable from './components/LearnersTable';
 import TableSearchFilter from '../../components/shared/TableSearchFilter';
+import { Button } from '@/components/ui/button';
+import FilterUserStatus from './components/FilterUserStatus';
 
 const ManageLearnersPage: FC = () => {
     return (
         <div className='bg-white rounded-xl min-h-[calc(100vh-117px)] flex flex-col text-primary'>
             <div className='py-4 px-5 flex items-center justify-between'>
                 <h2 className='font-semibold text-2xl'>Learners</h2>
-                <TableSearchFilter />
+                <div className='flex gap-3'>
+                    <TableSearchFilter />
+                    <FilterUserStatus />
+                </div>
             </div>
             <LearnersTable />
         </div>
