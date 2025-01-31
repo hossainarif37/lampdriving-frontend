43c59222b8fe99abbb6ece422b168af3cf124f7f
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-01-31 23:29:43 +0600

    feat: Refactor Change Password component to use react-hook-form for validation and error handling

--- src/app/(auth)/forgot-password/components/ChangePassword.tsx
+++ src/app/(auth)/forgot-password/components/ChangePassword.tsx
@@ -5,6 +5,7 @@ import { useResetPasswordMutation } from '@/redux/api/authApi/authApi';
 import { Eye, EyeClosed, KeyRound, Lock } from 'lucide-react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { Dispatch, FC, SetStateAction, useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 
 interface IChangePasswordProps {
@@ -13,24 +14,26 @@ interface IChangePasswordProps {
 
 const ChangePassword: FC<IChangePasswordProps> = ({ setSuccess }) => {
     const router = useRouter()
-    const [password, setPassword] = useState("")
-    const [confirmPassword, setConfirmPassword] = useState("")
     const [passwordVisible, setPasswordVisible] = useState(false);
     const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
-    const [error, setError] = useState<string>("")
+    const [confirmPasswordError, setConfirmPasswordError] = useState<string>("");
+
     const [resetPassword, { isLoading }] = useResetPasswordMutation();
     const searchParams = useSearchParams();
     const token = searchParams.get('token');
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault()
-        if (password !== confirmPassword) {
-            setError("Passwords do not match")
+
+    const { register, handleSubmit, formState: { errors }, watch } = useForm<{ password: string, confirmPassword: string }>();
+    const password = watch('password');
+    const confirmPassword = watch('confirmPassword');
+
+    const handleChangePassword = (data: { password: string, confirmPassword: string }) => {
+        if (data.password !== data.confirmPassword) {
+            setConfirmPasswordError("Passwords do not match");
             return;
         } else if (!token) {
             return;
         }
-
-        resetPassword({ newPassword: password, token: token }).unwrap().then((res) => {
+        resetPassword({ newPassword: data.password, token: token }).unwrap().then((res) => {
             toast({
                 message: res.message
             })
@@ -59,48 +62,61 @@ const ChangePassword: FC<IChangePasswordProps> = ({ setSuccess }) => {
             </div>
             <h1 className="text-2xl font-bold text-center text-primary/90">Change Password</h1>
             <p className="mb-2 text-sm text-accent text-center">Setup Your New Password</p>
-            <form onSubmit={handleSubmit} className="space-y-4">
-                <div className='w-full relative flex'>
-                    <Input
-                        type={passwordVisible ? "text" : "password"}
-                        placeholder="New password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                        className='w-full xl:h-12 mt-1 pr-10'
-                    />
-                    <span
-                        className='cursor-pointer absolute right-2 top-1/2 -translate-y-1/2'
-                        onClick={() => handlePasswordToggle('password')}
-                    >
-                        {passwordVisible ? <Eye width={20} height={20} /> : <EyeClosed width={20} height={20} />}
-                    </span>
+            <form onSubmit={handleSubmit(handleChangePassword)} className="space-y-4">
+                <div className='w-full'>
+                    <label htmlFor="password" className='font-semibold text-primary'>Password</label>
+                    <div className='w-full relative flex'>
+                        <Input
+                            {...register('password', {
+                                minLength: {
+                                    message: "Password must be at least 6 characters", value: 6
+                                },
+                                required: "Password is required"
+                            })}
+                            type={passwordVisible ? "text" : "password"}
+                            id='password'
+                            placeholder="Enter your password"
+                            className='w-full xl:h-12 mt-1 pr-10'
+                        />
+                        <span
+                            className='cursor-pointer absolute right-2 top-1/2 -translate-y-1/2'
+                            onClick={() => handlePasswordToggle('password')}
+                        >
+                            {passwordVisible ? <Eye width={20} height={20} /> : <EyeClosed width={20} height={20} />}
+                        </span>
+                    </div>
+                    {errors?.password && <p className='text-red-500 text-sm mt-1'>{errors?.password?.message}</p>}
                 </div>
-                <div className='w-full relative flex'>
-                    <Input
-                        type={confirmPasswordVisible ? "text" : "password"}
-                        placeholder="Confirm new password"
-                        value={confirmPassword}
-                        onChange={(e) => setConfirmPassword(e.target.value)}
-                        required
-                        className='w-full xl:h-12 mt-1 pr-10'
-                    />
-                    <span
-                        className='cursor-pointer absolute right-2 top-1/2 -translate-y-1/2'
-                        onClick={() => handlePasswordToggle('confirm-password')}
-                    >
-                        {confirmPasswordVisible ? <Eye width={20} height={20} /> : <EyeClosed width={20} height={20} />}
-                    </span>
+
+                <div className='w-full'>
+                    <label htmlFor="confirm-password" className='font-semibold text-primary'>Confirm Password</label>
+                    <div className='w-full relative flex'>
+                        <Input
+                            {...register('confirmPassword', {
+                                minLength: {
+                                    message: "Password must be at least 6 characters", value: 6
+                                },
+                                required: "Confirm Password is required"
+                            })}
+                            type={confirmPasswordVisible ? "text" : "password"}
+                            id='confirm-password'
+                            placeholder="Re-type your password"
+                            className='w-full xl:h-12 mt-1 pr-10'
+                        />
+                        <span
+                            className='cursor-pointer absolute right-2 top-1/2 -translate-y-1/2'
+                            onClick={() => handlePasswordToggle('confirm-password')}
+                        >
+                            {confirmPasswordVisible ? <Eye width={20} height={20} /> : <EyeClosed width={20} height={20} />}
+                        </span>
+                    </div>
+                    {errors?.confirmPassword && <p className='text-red-500 text-sm mt-1'>{errors.confirmPassword.message}</p>}
+                    {confirmPasswordError && <p className='text-red-500 text-sm mt-1'>{confirmPasswordError}</p>}
                 </div>
-                <div>
-                    {
-                        error &&
-                        <p className='text-red-500 mb-2'>{error}</p>
-                    }
-                    <Button loading={isLoading} disabled={isLoading || (!password || !confirmPassword)} className="w-full" type="submit">
-                        Confirm
-                    </Button>
-                </div>
+
+                <Button loading={isLoading} disabled={isLoading || (!password || !confirmPassword)} className="w-full mt-6" type="submit">
+                    Confirm
+                </Button>
             </form>
         </div>
     );
