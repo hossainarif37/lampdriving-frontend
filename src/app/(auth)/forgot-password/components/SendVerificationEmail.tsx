4183651580570354e0626c004e1523d62d2eb4d2
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-01-31 18:36:42 +0600

    feat: Implement Send Verification Email component for Forgot Password flow

---
+++ src/app/(auth)/forgot-password/components/SendVerificationEmail.tsx
@@ -0,0 +1,43 @@
+import { FC, useState } from 'react';
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Lock } from "lucide-react"
+import { useRouter } from 'next/navigation';
+
+const SendVerificationEmail: FC = () => {
+    const router = useRouter()
+
+    const [email, setEmail] = useState("")
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault()
+        console.log("Sending reset OTP to:", email)
+        router.push("/forgot-password/verify-otp")
+    }
+    return (
+        <div className="w-full md:w-[450px] xl:w-[500px] max-w-[500px] mx-auto p-10  md:shadow-lg md:rounded-lg md:border">
+            <div className="flex items-center justify-center mb-3">
+                <span className="p-3 bg-secondary/40 rounded-full flex  items-center justify-center">
+                    <Lock className="text-primary size-10 " />
+                </span>
+            </div>
+            <h1 className="text-2xl font-bold text-primary/90 text-center mb-4">Forgot Password</h1>
+            <p className="mb-2 text-sm text-accent">Enter your email. We will send you a OTP for verification.</p>
+            <form onSubmit={handleSubmit} className="space-y-6">
+                <Input
+                    type="email"
+                    placeholder="Enter your email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    className='w-full xl:h-12 mt-1 pr-10'
+                />
+                <Button className="w-full" type="submit">
+                    Next
+                </Button>
+            </form>
+        </div>
+    );
+};
+
+export default SendVerificationEmail;
--- src/app/(auth)/forgot-password/page.tsx
+++ src/app/(auth)/forgot-password/page.tsx
@@ -1,44 +1,13 @@
 "use client"
-import { useState, FC } from "react"
-import { useRouter } from "next/navigation"
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Lock } from "lucide-react"
+import { FC } from "react"
+import SendVerificationEmail from "./components/SendVerificationEmail"
 
 
 const ForgotPassword: FC = () => {
-    const router = useRouter()
-    const [email, setEmail] = useState("")
-
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault()
-        console.log("Sending reset OTP to:", email)
-        router.push("/forgot-password/verify-otp")
-    }
+    
     return (
         <div className="min-h-[calc(100vh-56px)] flex items-center justify-center">
-            <div className="w-full md:w-[450px] xl:w-[500px] max-w-[500px] mx-auto p-10  md:shadow-lg md:rounded-lg md:border">
-                <div className="flex items-center justify-center mb-3">
-                    <span className="p-3 bg-secondary/40 rounded-full flex  items-center justify-center">
-                        <Lock className="text-primary size-10 " />
-                    </span>
-                </div>
-                <h1 className="text-2xl font-bold text-primary/90 text-center mb-4">Forgot Password</h1>
-                <p className="mb-2 text-sm text-accent">Enter your email. We will send you a OTP for verification.</p>
-                <form onSubmit={handleSubmit} className="space-y-6">
-                    <Input
-                        type="email"
-                        placeholder="Enter your email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                        className='w-full xl:h-12 mt-1 pr-10'
-                    />
-                    <Button className="w-full" type="submit">
-                        Next
-                    </Button>
-                </form>
-            </div>
+            <SendVerificationEmail />
         </div>
     );
 };
