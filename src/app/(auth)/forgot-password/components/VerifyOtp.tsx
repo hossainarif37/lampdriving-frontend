58014d17b58afc03662c4cdbd50e4a6ae1cf844c
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-01-31 20:20:24 +0600

    feat: Integrate OTP input component for Verify OTP functionality in Forgot Password flow

--- src/app/(auth)/forgot-password/components/VerifyOtp.tsx
+++ src/app/(auth)/forgot-password/components/VerifyOtp.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
+import { InputOTP, InputOTPGroup, InputOTPSeparator, InputOTPSlot } from '@/components/ui/input-otp';
 import { useRouter } from 'next/navigation';
 import { FC, useState } from 'react';
 
@@ -14,18 +14,24 @@ const VerifyOtp: FC = () => {
     };
     return (
         <div className="w-full md:w-[450px] xl:w-[500px] max-w-[500px] p-3 md:p-10 md:shadow-lg md:rounded-md md:border">
-            <h1 className="text-2xl font-bold mb-4 text-center text-primary/90">Verify OTP</h1>
-            <p className="mb-2 text-sm">Enter the OTP from your reset password email.</p>
+            <h1 className="text-2xl font-bold mb-1 text-center text-primary/90">Verify OTP</h1>
+            <p className="mb-2 text-sm text-center">Enter the OTP from your reset password email.</p>
             <form onSubmit={handleSubmit} className="space-y-6">
-                <Input
-                    type="text"
-                    placeholder="Enter 6-digit OTP"
-                    value={otp}
-                    onChange={(e) => setOtp(e.target.value)}
-                    required
-                    maxLength={6}
-                    className='w-full xl:h-12 mt-1 pr-10'
-                />
+                <div className='text-center flex items-center justify-center mt-4'>
+                    <InputOTP maxLength={6} onChange={setOtp}>
+                        <InputOTPGroup>
+                            <InputOTPSlot index={0} />
+                            <InputOTPSlot index={1} />
+                            <InputOTPSlot index={2} />
+                        </InputOTPGroup>
+                        <InputOTPSeparator />
+                        <InputOTPGroup>
+                            <InputOTPSlot index={3} />
+                            <InputOTPSlot index={4} />
+                            <InputOTPSlot index={5} />
+                        </InputOTPGroup>
+                    </InputOTP>
+                </div>
                 <Button className="w-full" type="submit">
                     Verify
                 </Button>
