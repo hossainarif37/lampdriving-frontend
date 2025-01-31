a632ab6941f060eaa88f2f3e58bd91c35e617339
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-01-31 18:32:28 +0600

    feat: Revamp Forgot Password page layout and update button text for clarity

--- src/app/(auth)/forgot-password/page.tsx
+++ src/app/(auth)/forgot-password/page.tsx
@@ -3,6 +3,7 @@ import { useState, FC } from "react"
 import { useRouter } from "next/navigation"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
+import { Lock } from "lucide-react"
 
 
 const ForgotPassword: FC = () => {
@@ -15,22 +16,29 @@ const ForgotPassword: FC = () => {
         router.push("/forgot-password/verify-otp")
     }
     return (
-        <div className="w-full md:w-[450px] xl:w-[500px] max-w-[500px] mx-auto mt-32 p-6 border md:shadow-lg md:rounded-md md:border">
-            <h1 className="text-2xl font-bold text-primary/90 text-center mb-4">Forgot Password</h1>
-            <p className="mb-2 text-sm text-accent">Enter your email to receive a password reset OTP.</p>
-            <form onSubmit={handleSubmit} className="space-y-6">
-                <Input
-                    type="email"
-                    placeholder="Enter your email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                    className='w-full xl:h-12 mt-1 pr-10'
-                />
-                <Button className="w-full" type="submit">
-                    Send Reset OTP
-                </Button>
-            </form>
+        <div className="min-h-[calc(100vh-56px)] flex items-center justify-center">
+            <div className="w-full md:w-[450px] xl:w-[500px] max-w-[500px] mx-auto p-10  md:shadow-lg md:rounded-lg md:border">
+                <div className="flex items-center justify-center mb-3">
+                    <span className="p-3 bg-secondary/40 rounded-full flex  items-center justify-center">
+                        <Lock className="text-primary size-10 " />
+                    </span>
+                </div>
+                <h1 className="text-2xl font-bold text-primary/90 text-center mb-4">Forgot Password</h1>
+                <p className="mb-2 text-sm text-accent">Enter your email. We will send you a OTP for verification.</p>
+                <form onSubmit={handleSubmit} className="space-y-6">
+                    <Input
+                        type="email"
+                        placeholder="Enter your email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                        className='w-full xl:h-12 mt-1 pr-10'
+                    />
+                    <Button className="w-full" type="submit">
+                        Next
+                    </Button>
+                </form>
+            </div>
         </div>
     );
 };
