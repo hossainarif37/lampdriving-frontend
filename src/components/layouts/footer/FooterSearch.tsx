d690b5ecc370a33afbdf41006eee0b64cacf63d8
Hridoy Ahmed <basirulislam.hridoy05@gmail.com>
2025-02-06 23:08:22 +0600

    fix: add missing line break in FooterSearch component

--- src/components/layouts/footer/FooterSearch.tsx
+++ src/components/layouts/footer/FooterSearch.tsx
@@ -52,6 +52,7 @@ const FooterSearch: FC = () => {
             })
             .catch((err) => console.error("Error fetching suburbs:", err));
     };
+
     return (
         <div className="lg:max-w-5xl mx-auto md:max-w-3xl h-48 md:h-40 flex flex-col md:flex-row items-center justify-center md:gap-4 gap-6 w-full md:rounded-md rounded-lg md:px-20 px-6 bg-[#264649] -translate-y-1/2">
 
