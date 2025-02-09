f8d3f748434f21251a320e0a08368fc34d893f7e
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-02-09 15:27:00 +0600

    feat: update ScheduleStep component to enhance scheduling options and improve UI layout

--- src/app/(root)/instructors/booking/components/schedule-step/ScheduleStep.tsx
+++ src/app/(root)/instructors/booking/components/schedule-step/ScheduleStep.tsx
@@ -165,30 +165,52 @@ const ScheduleStep: FC = () => {
             <div className="md:grid space-y-4 sm:space-y-6 md:space-y-0 grid-cols-1 lg:grid-cols-2 gap-4 sm:gap-6">
                 <div className="space-y-6 col-span-2">
                     <div className="bg-white rounded-lg shadow-sm  p-4 lg:p-6 border border-gray-200 relative">
-                        <h2 className="text-lg font-semibold mb-4">Select Duration</h2>
-                        <div className="flex gap-4 flex-wrap">
-                            {[1, 2].map((duration) => (
-                                <button
-                                    key={duration}
-                                    disabled={duration > availableScheduleHours}
-                                    onClick={() => handleDuration(duration as 1 | 2, "lesson")}
-                                    className={`flex-1 py-2 px-4 rounded-[4px] border disabled:text-gray-500 ${(selectedSchedule.duration === duration && selectedSchedule.type === "lesson")
-                                        ? 'border-primary bg-primary/5 text-primary'
-                                        : 'border-gray-200 hover:border-primary/70'
-                                        }`}
-                                >
-                                    {duration}-Hour Lesson
-                                </button>
-                            ))}
+                        <h2 className="text-lg font-semibold mb-4">Select Schedule Type</h2>
+                        <div className="flex flex-col md:grid grid-cols-3 gap-4">
+                            <div className='flex flex-col sm:flex-row items-center gap-4 col-span-3'>
+                                {[1, 2].map((duration) => (
+                                    <button
+                                        key={duration}
+                                        disabled={duration > availableScheduleHours}
+                                        onClick={() => handleDuration(duration as 1 | 2, "lesson")}
+                                        className={`flex-1 py-2 px-4 rounded-[4px] border w-full disabled:text-gray-500 ${(selectedSchedule.duration === duration && selectedSchedule.type === "lesson")
+                                            ? 'border-primary bg-primary/5 text-primary'
+                                            : 'border-gray-200 hover:border-primary/70'
+                                            }`}
+                                    >
+                                        {duration}-Hour Lesson
+                                    </button>
+                                ))}
+                            </div>
                             <button
                                 disabled={!testPackage.included || isTestPackageSelected}
                                 onClick={() => handleDuration(2, "test")}
-                                className={`flex-1 py-2 px-4 rounded-[4px] disabled:text-gray-500 border ${selectedSchedule.type === "test"
+                                className={`py-2 px-4 text-nowrap rounded-[4px] disabled:text-gray-500 border ${selectedSchedule.type === "test"
                                     ? 'border-primary bg-primary/5 text-primary'
                                     : 'border-gray-200 hover:border-primary/70'
                                     }`}
                             >
-                                Test Package
+                                Driving Test
+                            </button>
+                            <button
+                                disabled={!testPackage.included || isTestPackageSelected}
+                                onClick={() => handleDuration(2, "test")}
+                                className={`py-2 px-4 text-nowrap rounded-[4px] disabled:text-gray-500 border ${selectedSchedule.type === "test"
+                                    ? 'border-primary bg-primary/5 text-primary'
+                                    : 'border-gray-200 hover:border-primary/70'
+                                    }`}
+                            >
+                                First Mock Test
+                            </button>
+                            <button
+                                disabled={!testPackage.included || isTestPackageSelected}
+                                onClick={() => handleDuration(2, "test")}
+                                className={`py-2 px-4 text-nowrap rounded-[4px] disabled:text-gray-500 border ${selectedSchedule.type === "test"
+                                    ? 'border-primary bg-primary/5 text-primary'
+                                    : 'border-gray-200 hover:border-primary/70'
+                                    }`}
+                            >
+                                Mock Test
                             </button>
                         </div>
                         <button title={availableScheduleHours === 0 ? 'No hours left to schedule' : `Add more ${availableScheduleHours} ${availableScheduleHours === 1 ? 'hour' : 'hours'} schedules`} className='absolute top-4 right-4 sm:top-6 sm:right-6 flex items-center gap-2'>
