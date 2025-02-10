ab3ba02d433a49c960b777aa9f93f5d7e0d23af5
Mahmudul Hasan <mahmudulhasanturan@gmail.com>
2025-02-10 12:16:38 +0600

    refactor: extract schedule type selection into a separate component for improved modularity

--- src/app/(root)/instructors/booking/components/schedule-step/ScheduleStep.tsx
+++ src/app/(root)/instructors/booking/components/schedule-step/ScheduleStep.tsx
@@ -7,12 +7,12 @@ import { format } from 'date-fns';
 import { useBooking } from '@/providers/BookingProvider';
 import DropOffLocation from './DropOffLocation';
 import { IWorkingHour } from '@/types/instructor';
-import { CircleAlert } from 'lucide-react';
 import { useGetInstructorAvailabilityQuery } from '@/redux/api/scheduleApi/scheduleApi';
 import { IScheduleInputs } from '@/types/schedule';
 import { IAddress } from '@/types/user';
 import { useAppDispatch } from '@/redux/hook';
 import { showNotification } from '@/redux/slices/notificationSlice/notificationSlice';
+import SelectScheduleType from './SelectScheduleType';
 
 interface ISelectedSchedule {
     date: Date | null;
@@ -94,7 +94,7 @@ const ScheduleStep: FC = () => {
         }
     };
 
-
+    // handle schedule type automatically based on available schedule hours
     useEffect(() => {
         if (availableScheduleHours > 1) {
             setSelectedSchedule((pre) => ({ ...pre, duration: selectedSchedule.type === "test" ? 1 : selectedSchedule.duration, type: "lesson" }));
@@ -172,109 +172,17 @@ const ScheduleStep: FC = () => {
         <div className="space-y-6 sticky top-10">
             <div className="md:grid space-y-4 sm:space-y-6 md:space-y-0 grid-cols-1 lg:grid-cols-2 gap-4 sm:gap-6">
                 <div className="space-y-6 col-span-2">
-                    <div className="bg-white rounded-lg shadow-sm  p-4 lg:p-6 border border-gray-200 relative">
-                        <h2 className="text-lg font-semibold mb-4">Select Schedule Type</h2>
-                        <div className="flex flex-col md:grid grid-cols-3 gap-4">
-                            <div className='flex flex-col sm:flex-row items-center gap-4 col-span-3'>
-                                {[1, 2].map((duration) => (
-                                    <button
-                                        key={duration}
-                                        disabled={duration > availableScheduleHours}
-                                        onClick={() => handleDuration(duration as 1 | 2, "lesson")}
-                                        className={`flex-1 py-2 px-4 rounded-[4px] border w-full disabled:text-gray-500 ${(selectedSchedule.duration === duration && selectedSchedule.type === "lesson")
-                                            ? 'border-primary bg-primary/5 text-primary'
-                                            : 'border-gray-200 hover:border-primary/70'
-                                            }`}
-                                    >
-                                        {duration}-Hour Lesson
-                                    </button>
-                                ))}
-                            </div>
-                            <button
-                                disabled={!testPackage.included || isTestPackageSelected}
-                                onClick={() => handleDuration(2, "test")}
-                                className={`py-2 px-4 text-nowrap rounded-[4px] disabled:text-gray-500 border ${selectedSchedule.type === "test"
-                                    ? 'border-primary bg-primary/5 text-primary'
-                                    : 'border-gray-200 hover:border-primary/70'
-                                    }`}
-                            >
-                                Driving Test
-                            </button>
-                            <button
-                                disabled={!(testPackage.mockTestCount) || isFirstMockTestScheduled}
-                                onClick={() => handleDuration(2, "mock-test")}
-                                className={`py-2 px-4 text-nowrap rounded-[4px] disabled:text-gray-500 border ${selectedSchedule.type === "mock-test" && selectedSchedule.duration === 2
-                                    ? 'border-primary bg-primary/5 text-primary'
-                                    : 'border-gray-200 hover:border-primary/70'
-                                    }`}
-                            >
-                                First Mock Test
-                            </button>
-                            <button
-                                disabled={!(testPackage.mockTestCount) || isAllMockTestScheduled}
-                                onClick={() => handleDuration(1, "mock-test")}
-                                className={`py-2 px-4 text-nowrap rounded-[4px] disabled:text-gray-500 border ${selectedSchedule.type === "mock-test" && selectedSchedule.duration === 1
-                                    ? 'border-primary bg-primary/5 text-primary'
-                                    : 'border-gray-200 hover:border-primary/70'
-                                    }`}
-                            >
-                                Mock Test
-                            </button>
-                        </div>
-                        <div className='absolute top-4 right-4 sm:top-6 sm:right-6'>
-                            {
-                                selectedSchedule.type === "lesson" ?
-                                    <button
-                                        title={availableScheduleHours === 0 ? 'No hours left to schedule' : `Add more ${availableScheduleHours} ${availableScheduleHours === 1 ? 'hour' : 'hours'} schedules`}
-                                        className='flex items-center gap-2'
-                                    >
-                                        <span className='text-sm'>
-                                            {availableScheduleHours === 0 ? 'No hours left to schedule' : `${availableScheduleHours}-Hours left`}
-                                        </span>
-                                        <CircleAlert size={16} />
-                                    </button>
-                                    :
-                                    (selectedSchedule.type === "test" && !isTestPackageSelected) ?
-                                        <button
-                                            title='Schedule driving test'
-                                            className='flex items-center gap-2'
-
-                                        >
-                                            <span className='text-sm'>Schedule driving test</span>
-                                            <CircleAlert size={16} />
-                                        </button>
-                                        :
-                                        (selectedSchedule.type === "mock-test" && selectedSchedule.duration === 2 && !isFirstMockTestScheduled) ?
-                                            <button
-                                                title='Schedule first mock test'
-                                                className='flex items-center gap-2'
-                                            >
-                                                <span className='text-sm'>Schedule first mock test</span>
-                                                <CircleAlert size={16} />
-                                            </button>
-                                            :
-                                            (selectedSchedule.type === "mock-test" && selectedSchedule.duration === 1 && !isAllMockTestScheduled) ?
-                                                <button
-                                                    title='Schedule all mock tests'
-                                                    className='flex items-center gap-2'
-
-                                                >
-                                                    <span className='text-sm'>Schedule all mock tests</span>
-                                                    <CircleAlert size={16} />
-                                                </button>
-                                                :
-                                                <button
-                                                    title='Nothing is left to schedule'
-                                                    className='flex items-center gap-2'
-
-                                                >
-                                                    <span className='text-sm'>Nothing is left to schedule</span>
-                                                    <CircleAlert size={16} />
-                                                </button>
-                            }
-                        </div>
-                    </div>
+                    <SelectScheduleType
+                        availableScheduleHours={availableScheduleHours}
+                        isTestPackageSelected={isTestPackageSelected}
+                        isFirstMockTestScheduled={isFirstMockTestScheduled}
+                        isAllMockTestScheduled={isAllMockTestScheduled}
+                        handleDuration={handleDuration}
+                        selectedSchedule={selectedSchedule}
+                        testPackage={testPackage}
+                    />
                 </div>
+
 
                 <div>
                     <ScheduleCalender
